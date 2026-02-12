cd8ac5db113258b368e0324816ee4f09171fa830
Srikanth Mohan <srikanth.mohan.98@gmail.com>
2026-02-12 17:58:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function stringChop(str, size) {
-  // your code here
+  if (str==null) return [];
+	size=Number(size);
+	let result=[];
+	for(let i=0;i<str.length;i+=size){
+		let end=Math.min(i+size,str.length);
+		result.push(str.slice(i,end));	
+	}
+	return result
 }
 
 // Do not change the code below
